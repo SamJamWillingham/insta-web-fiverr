ff7ab0b9e2a344b03e7eaf8ece807a07801e4b42
Sam Willingham <68390031+SamJamWillingham@users.noreply.github.com>
2020-12-07 13:30:35 -0500

    added media queries for mobile devices; small styling adjustments

--- my-app/src/links.jsx
+++ my-app/src/links.jsx
@@ -7,7 +7,7 @@ function Links() {
     <div className="links">
         <div className="card">
             <img src={undrawChat} alt="chat illustration" width={200}/>
-<h2>proofread and edit my content</h2>
+<h3>proofread and edit my content</h3>
     <a href="#">
      <button>
     Fiverr
@@ -16,7 +16,7 @@ function Links() {
         </div>
     <div className="card">
         <img src={undrawLearning} alt="chat illustration" width={200}/>
-    <h2>proofread and edit my lesson plans</h2>
+    <h3>proofread and edit my lesson plans</h3>
     <a href="#">
      <button>
     Fiverr
@@ -25,7 +25,7 @@ function Links() {
     </div>
     <div className="card">
         <img src={undrawHelp} alt="chat illustration" width={200}/>
-    <h2>SOS! Help me with my lesson plans NOW!</h2>
+    <h3>SOS! Help me with my lesson plans NOW!</h3>
     <a href="mailto:samanthalf8@gmail.com" subject="SOS!%Can%we%meet%on%Zoom?">
      <button>
     Email
